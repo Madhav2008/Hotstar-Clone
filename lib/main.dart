5370ecacaa522e9b0e27c9fe1f35757f6683a174
arora <aroramadhav65@gmail.com>
2022-05-19 23:25:43 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -72,8 +72,8 @@ class _MyAppState extends State<MyApp> {
             Spacer(),
             Image.asset('../assets/images/banner3.webp'),
             Spacer(),
-            Spacer(),
             Image.asset('../assets/images/banner4.webp'),
+            Spacer(),
           ],
           options: CarouselOptions(
             autoPlay: true, viewportFraction: 0.8,
