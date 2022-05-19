5ca8107340e56373e6d8338533370193abb1be40
arora <aroramadhav65@gmail.com>
2022-05-19 23:29:19 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyAppState extends State<MyApp> {
             Image.asset(
               '../assets/images/banner1.webp',
             ),
-            SizedBox(width: ,)
+            SizedBox(wi)
             Image.asset(
               '../assets/images/banner2.webp',
             ),
