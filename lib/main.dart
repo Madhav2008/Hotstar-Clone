365dd67c97cd216a4d44d1e575fe56ceefbb5a21
arora <aroramadhav65@gmail.com>
2022-05-19 23:16:54 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyAppState extends State<MyApp> {
         CarouselSlider(
           items: [
             Image.asset('../assets/images/banner1.webp'),
-            Image.asset('assets/images/banner2.webp'),
+            Image.asset('../assets/images/banner2.webp'),
             Image.asset('assets/images/banner3.webp'),
             Image.asset('assets/images/banner4.webp'),
           ],
