e4a548f480cf48af3a43e27afac307a676e06930
arora <aroramadhav65@gmail.com>
2022-05-19 23:27:45 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyAppState extends State<MyApp> {
             Image.asset(
               '../assets/images/banner1.webp',
             ),
-            Image.asset('../assets/images/banner2.webp'),
+            Image.asset('../assets/images/banner2.webp',),
             Image.asset('../assets/images/banner3.webp'),
             Image.asset('../assets/images/banner4.webp'),
           ],
