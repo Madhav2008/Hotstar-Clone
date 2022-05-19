deab359bb25a04b5c44854ec4663c49ead93028a
arora <aroramadhav65@gmail.com>
2022-05-19 23:46:30 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MaterialApp(
