06dff4f8c07e4396e1aa9935bf98d5076f0470f6
arora <aroramadhav65@gmail.com>
2022-05-20 19:08:57 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -516,7 +516,7 @@ class _MarvelState extends State<Marvel> {
                   options: CarouselOptions(
                       enableInfiniteScroll: false,
                       viewportFraction: 0.3,
-                      initialPage: 1)),
+                      initialPage: 1,)),
               Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
                 Text(
                   'Marvel Legacy Animation',
