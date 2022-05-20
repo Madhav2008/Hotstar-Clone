bf71fbf7b7418fbb3b5487bd06d08e3c147f9fd8
arora <aroramadhav65@gmail.com>
2022-05-20 19:09:28 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -531,7 +531,7 @@ class _MarvelState extends State<Marvel> {
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-              ]),
+              ],),
               CarouselSlider(
                   items: [
                     TextButton(
