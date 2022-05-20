aeeca1506ed418e60b33894dfe43433d442b09b3
arora <aroramadhav65@gmail.com>
2022-05-20 20:03:02 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -995,7 +995,7 @@ class _LangState extends State<Lang> {
                       ),
                     ],
                   ),
-                ]),
+                ],),
                 Stack(alignment: AlignmentDirectional.center, children: [
                   Container(
                     child: Image.asset('assets/images/lang4.webp'),
