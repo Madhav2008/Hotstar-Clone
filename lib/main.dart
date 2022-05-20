a4c253ef58d389f8ec6cd34d3e6e99a8754cfa4f
arora <aroramadhav65@gmail.com>
2022-05-20 19:10:27 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -594,26 +594,27 @@ class _PixarState extends State<Pixar> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            actions: [
-              FloatingActionButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(Icons.arrow_back),
-              )
-            ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Icon(Icons.arrow_back),
+            )
+          ],
+        ),
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            'Pixar page',
+            style: TextStyle(color: Colors.red),
           ),
-          backgroundColor: Colors.black,
-          body: Center(
-            child: Text(
-              'Pixar page',
-              style: TextStyle(color: Colors.red),
-            ),
-          ),
-        ),);
+        ),
+      ),
+    );
   }
 }
 
